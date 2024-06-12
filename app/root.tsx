e6e32b0348017952f4bf06caf09a49cca65c7be6
Kevin Perez <kp952547@gmail.com>
2024-06-12 15:24:04 -0600

    patch: fixing stylesheet issue

--- app/root.tsx
+++ app/root.tsx
@@ -6,8 +6,9 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
-import stylesheet from "./styles/tailwind.css";
 import NavBar from "./components/navbar";
+
+import stylesheet from "./styles/tailwind.css";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
