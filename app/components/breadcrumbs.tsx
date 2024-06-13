6457d29f5bc039c541fd8ac09f00e32b7387c965
Kevin Perez <kp952547@gmail.com>
2024-06-12 18:10:18 -0600

    updated breadcrumb logic

--- app/components/breadcrumbs.tsx
+++ app/components/breadcrumbs.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link, useLocation } from '@remix-run/react';
+import ImageSwitcher from './imageSwitcher';
+import { ChevronRightIcon } from '@heroicons/react/20/solid';
 
 const Breadcrumbs: React.FC = () => {
   const location = useLocation();
@@ -7,26 +9,48 @@ const Breadcrumbs: React.FC = () => {
 
   return (
 	<nav className="w-full">
-	  <ol className="list-reset flex">
-		<li>
-		  <Link to="/" className="opacity-80">Home</Link>
-		</li>
-		{pathnames.map((value, index) => {
-		  const to = `/${pathnames.slice(0, index + 1).join('/')}`;
-		  const isLast = index === pathnames.length - 1;
-		  return (
-			<li key={to} className="flex items-center">
-			  <span className="">/</span>
-			  {isLast ? (
-				<span className="">{value}</span>
-			  ) : (
-				<Link to={to} className="">
-				  {value}
-				</Link>
-			  )}
+	  <ol className="list-reset flex flex-row gap-2.5 items-center">
+		{location.pathname === "/" ? (
+		  <li className='flex flex-row gap-2.5 items-center'>
+			<Link to="/" className="opacity-80">
+			  <ImageSwitcher
+				lightImgSrc='swift-icon-light.svg'
+				darkImgSrc='swift-icon-dark.svg'
+				altText='Swift Bird Icon'
+			  />
+			</Link>
+			<ChevronRightIcon className="h-5 w-5 text-gray-500 mx-2" />
+			<Link to="/" className="opacity-80">Overview</Link>
+		  </li>
+		) : (
+		  <>
+			<li className='flex flex-row gap-2.5 items-center'>
+			  <Link to="/" className="opacity-80">
+				<ImageSwitcher
+				  lightImgSrc='swift-icon-light.svg'
+				  darkImgSrc='swift-icon-dark.svg'
+				  altText='Swift Bird Icon'
+				/>
+			  </Link>
 			</li>
-		  );
-		})}
+			{pathnames.map((value, index) => {
+			  const to = `/${pathnames.slice(0, index + 1).join('/')}`;
+			  const isLast = index === pathnames.length - 1;
+			  return (
+				<li key={to} className="flex items-center">
+				  <ChevronRightIcon className="h-5 w-5 text-gray-500 mx-2" />
+				  {isLast ? (
+					<span className="text-gray-500">{value}</span>
+				  ) : (
+					<Link to={to} className="text-blue-600 hover:text-blue-800">
+					  {value}
+					</Link>
+				  )}
+				</li>
+			  );
+			})}
+		  </>
+		)}
 	  </ol>
 	</nav>
   );
