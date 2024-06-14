a6147997dd913249e18a15b2e017c68943f19b16
Kevin Perez <kp952547@gmail.com>
2024-06-14 09:43:00 -0600

    update with google analytics tag

--- app/root.tsx
+++ app/root.tsx
@@ -4,16 +4,25 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 import NavBar from "./components/navbar";
 import Footer from "./components/footer";
+import { LoaderFunction } from "@remix-run/node";
 
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: "/styles/tailwind.css" }
+  { rel: "stylesheet", href: "/styles/tailwind.css" },
 ];
 
+export const loader: LoaderFunction = async () => {
+  return {
+    gaTrackingId: "G-Y5M0S6FWR8", // Replace with your GA tracking ID
+  };
+};
+
 export function Layout({ children }: { children: React.ReactNode }) {
+  const { gaTrackingId } = useLoaderData();
   return (
     <html lang="en">
       <head>
@@ -21,12 +30,28 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        {gaTrackingId && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+            ></script>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${gaTrackingId}');
+                  `,
+              }}
+            />
+          </>
+        )}
       </head>
       <body className="bg-white dark:bg-black text-custom-dark dark:text-white">
         <NavBar />
-        <div className="pt-16">
-          {children}
-        </div>
+        <div className="pt-16">{children}</div>
         <ScrollRestoration />
         <Scripts />
         <Footer />
